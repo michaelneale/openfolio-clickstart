47bba5d07cdea45d95731f0f78e1586632ad2c7b
Benjamin Savoy <thefrozenanthem@gmail.com>
2013-01-15 00:18:10 +0100

    added tmp dit

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -601,3 +601,5 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+$conf['file_temporary_path'] = getenv('TMPDIR');
